aba0dab87870ae1585f8b6619bd741264904b833
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-02-12 21:31:23 +0900

    Fix: reupload 15650_N_and_M_2

--- baekjoon/Backtracking/15650_N_and_M_2.py
+++ baekjoon/Backtracking/15650_N_and_M_2.py
@@ -1,4 +1,17 @@
-s = set([1,2])
-l = [2,1]
-s.add(l)
-print(s)
+n, m = list(map(int, input().split())) # 4 2
+l = []
+setList = []
+
+def dfs():
+    if len(l) == m and set(l) not in setList:
+        print(' '.join(map(str, l)))
+        setList.append(set(l))
+        return
+    
+    for i in range(1,n+1):
+        if i not in l:
+            l.append(i)
+            dfs()
+            l.pop()         # pop i
+        
+dfs()
